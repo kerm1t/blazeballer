979329256bc1e947b49a6cca1b2fc7c2743b60e7
kerm1t <wolfgang.schulz@gmail.com>
2022-12-11 11:42:28 +0100

    blendmode and camera_control :-)

--- p5js/grid.js
+++ p5js/grid.js
@@ -3,11 +3,49 @@
 // ballblazer
 // (c) 2022 kmt, mirage, ekwotech
 
+let sliderGroup = [];
+let X;
+let Y;
+let Z;
+let centerX;
+let centerY;
+let centerZ;
+let h = 20
+
+function setup_cam() {
+  //create sliders
+  for (var i = 0; i < 6; i++) {
+    if (i === 2) {
+      sliderGroup[i] = createSlider(10, 400, 200);
+    } else {
+      sliderGroup[i] = createSlider(-400, 400, 0);
+    }
+    h = map(i, 0, 6, 5, 85);
+    sliderGroup[i].position(10, height + h);
+    sliderGroup[i].style('width', '80px');
+  }
+  describe(
+    'White square repeatedly grows to fill canvas and then shrinks. An interactive example of a red cube with 3 sliders for moving it across x, y, z axis and 3 sliders for shifting its center.'
+  );
+}
+
+function set_cam() {
+  // assigning sliders' value to each parameters
+  X = sliderGroup[0].value();
+  Y = sliderGroup[1].value();
+  Z = sliderGroup[2].value();
+  centerX = sliderGroup[3].value();
+  centerY = sliderGroup[4].value();
+  centerZ = sliderGroup[5].value();
+  camera(X, Y, Z, centerX, centerY, centerZ, 0, 1, 0);  
+}
+
 function infinite_grid() {
   background(200);
 //  noFill();
   strokeWeight(0); // Thicker
- 
+  blendMode(SUBTRACT);
+  
   // ver slow now - however much faster in https://editor.p5js.org/
   for (let y=0;y<40;y++) {
     beginShape(QUAD_STRIP); // add only 2 new vertices
@@ -39,6 +77,7 @@ function zoom(event) {
 
 function setup() {
   cnv = createCanvas(1200, 800, WEBGL);
+  setup_cam();
 
 //  spiral();
 //  spiral3d(); tut's ned
@@ -51,6 +90,7 @@ function setup() {
 
 function draw()
 {
+  set_cam();
   yinc += 1; // 2do: to go slower or faster, have a scaling factor
   if (yinc%10==0)
   {
